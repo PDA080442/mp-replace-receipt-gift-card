3de2edfbd94352bddbd52a3e60cd58fcade2a5b6
Данил Поправкин <danilpopravkin@244080.local>
2026-04-07 17:26:42 +0700

    feat: HPOS and Woo API

--- mp-replace-receipt-gift-card.php
+++ mp-replace-receipt-gift-card.php
@@ -19,6 +19,19 @@ define( 'MP_REPLACE_RECEIPT_GIFT_CARD_VERSION', '0.0.1' );
 define( 'MP_RRGC_PLUGIN_FILE', __FILE__ );
 define( 'MP_RRGC_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
+add_action(
+	'before_woocommerce_init',
+	static function () {
+		if ( class_exists( '\Automattic\WooCommerce\Utilities\FeaturesUtil' ) ) {
+			\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility(
+				'custom_order_tables',
+				MP_RRGC_PLUGIN_FILE,
+				true
+			);
+		}
+	}
+);
+
 require_once MP_RRGC_PLUGIN_DIR . 'includes/class-mp-rrgc-settings.php';
 require_once MP_RRGC_PLUGIN_DIR . 'includes/class-mp-rrgc-logger.php';
 require_once MP_RRGC_PLUGIN_DIR . 'includes/class-mp-rrgc-gift-detector.php';
